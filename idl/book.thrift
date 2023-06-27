6e7931f3b81298c8acad9740482b18eb16e50751
Shawn <shawnnygoh@gmail.com>
2023-06-27 15:24:52 +0800

    fix: resolve syntax error in book.thrift

---
+++ idl/book.thrift
@@ -0,0 +1,28 @@
+namespace go book
+
+struct QueryBookReq {
+    1: i32 Num (api.query="num", api.vd="$<100; msg:'num must less than 100'");
+}
+
+struct QueryBookResp {
+    1: string ID;
+    2: string Title;
+    3: string Author;
+    4: string Content; 
+}
+
+struct InsertBookReq {
+    1: string ID (api.form="id");
+    2: string Title (api.form="title");
+    3: string Author (api.form="author");
+}
+
+struct InsertBookResp {
+    1: bool Ok; 
+    2: string Msg; 
+}
+
+service BookSvc {
+   QueryBookResp queryBook(1: QueryBookReq req) (api.get="book/query");
+   InsertBookResp insertBook(1: InsertBookReq req) (api.post="book/insert");
+}
